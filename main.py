cf1c692d11005b29643829d58e0a8c95ea055a42
roelej <roelej@ier.unam.mx>
2025-02-06 12:31:54 -0600

    Añadida función suma en utils.py y llamada en main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils import saludar, suma
+
+if __name__ == "__main__":
+    print(saludar("Mundo"))
+    print(f"2 + 3 = {suma(2, 3)}")
---
+++ utils.py
@@ -0,0 +1,5 @@
+def saludar(nombre):
+    return f"¡Hola, {nombre}!"
+
+def suma(a, b):
+    return a + b

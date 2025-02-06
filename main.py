5392bb43faa6211dfca7ce8573d35f03b70670b3
roelej <roelej@ier.unam.mx>
2025-02-06 12:37:28 -0600

    Añadida función resta en utils.py y llamada en main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils import saludar, suma
+from utils import saludar, suma, resta
 
 if __name__ == "__main__":
     print(saludar("Mundo"))
     print(f"2 + 3 = {suma(2, 3)}")
+    print(f"5 - 3 = {resta(5, 3)}")
--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ def saludar(nombre):
 
 def suma(a, b):
     return a + b
+
+def resta(a, b):
+    return a - b
